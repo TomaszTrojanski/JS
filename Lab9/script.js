5375a49a28ad637e9bb62f0e2dbb7a00bb14ed2f
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-12-15 20:55:38 +0100

    l9.api

--- Lab9/script.js
+++ Lab9/script.js
@@ -13,3 +13,21 @@ addBtn.addEventListener('click',function(){
 });
 
 remove.addEventListener('click', removeAllForecasts);
+
+function getWeatherData(){
+    let weather = {};
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=metric&appid=57cc083a05fa6a2008dc652336e25912')
+		.then((response) => response.json())
+		.then(data => {
+			weather.city = data['name'];
+			weather.desc = data['weather'][0]['description'];
+			weather.temp = Math.round(data['main']['temp']);
+			weather.image = data['weather'][0]['icon'];
+			weather.wind = data['wind']['speed'];
+			weather.pressure = data['main']['pressure'];		
+			forecasts.push(weather);	
+			localStorage.setItem(lsWeatherKey, JSON.stringify(forecasts));
+		})
+		.then(() => displayForecast(weather))
+		.catch(err => console.log(err));
+}
